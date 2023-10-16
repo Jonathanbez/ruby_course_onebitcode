f7e0c83c6f2056ffdb498b820628991556f02bd1
Jonathanbez <jbbarroz@gmail.com>
2023-10-16 10:11:18 -0300

    5nd course OneBitCode

--- control_structures/while.rb
+++ control_structures/while.rb
@@ -1,4 +1,12 @@
 # WHILE - ITERAÇÃO
 # WHILE (Enquanto) - Executa um bloco de codigo enquanto a condição for verdadeira
 
-alphabet = 
+letra = "a"
+buffer = ""
+
+while letra < "z"
+    buffer += letra
+    letra = letra.next
+end
+
+puts buffer
