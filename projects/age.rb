81b51dd16531d081beb77836d6d29544bcca1808
Jonathanbez <jbbarroz@gmail.com>
2023-10-18 07:42:43 -0300

    19nd course OneBitCode

--- projects/age.rb
+++ projects/age.rb
@@ -1,5 +1,6 @@
 # Calcular a idade
 
+require 'date'
 result = ' '
 
 loop do
@@ -13,10 +14,11 @@ loop do
     if option == 1
         print 'Digite a data de nascimento: '
         year_of_bith = gets.chomp
-        year_of_bith = Data.strptime(year_of_bith, "%d%mY%")
+        year_of_bith = Date.strptime(year_of_bith, "%d/%m/%Y")
         print 'Digite a data atual: '
-        corrent_year = get.chomp
-        corrent_year = Data.strptime(corrent_year, "%d%m%Y")
+        corrent_year = gets.chomp
+        corrent_year = Date.strptime(corrent_year, "%d/%m/%Y")
         age = current_year - year_of_bith
         result = "Quem nascne em #{year_of_bith}, tem #{age} de idade"
-        
+    end
+end
