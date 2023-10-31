ef7a12a9fe073bb3f243e4ce1504bc606648be5b
Jonathanbez <jbbarroz@gmail.com>
2023-10-31 07:19:18 -0300

    41nd course OneBitCode

--- collections/array.rb
+++ collections/array.rb
@@ -20,12 +20,13 @@ estados.insert(0, 'Acre', 'Amapá')
 
 # aqui inseri na terceira posição (lembrando que array começa na posição "0") 
 estados.insert(2, 'Ceara')
-puts estados[0]
 
 # consigo escolher a posição com [0..] para do inicio ao fim, 
 # ou [0..2] para o incio ate determinado ponto.
 # posso tambem [-2] para escolher de um ponto de tras pra frente. 
-# ou tambem do primeiro elemento do array 
-puts estados.last
-puts estados.first
-puts estados.count
+# ou tambem do primeiro, ultimo ou contar elemento do array 
+
+puts estados[0] #escolher a posição desejada elemento
+puts estados.last #ultimo elemento
+puts estados.first #primeir elemento
+puts estados.count #contar elemento
