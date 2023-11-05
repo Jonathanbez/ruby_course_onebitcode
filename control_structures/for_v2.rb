516f1d3b7d03982648fe58a6897ca4b143fd8938
Jonathanbez <jbbarroz@gmail.com>
2023-11-05 20:34:11 -0300

    45nd course OneBitCode

--- control_structures/for_v2.rb
+++ control_structures/for_v2.rb
@@ -1,15 +1,13 @@
-#FOR aninhado
+#WHILE aninhado
 
 number = 1
 
-number2 = 2
-
-while number >= 10
+while number <= 10
     puts number 
-    count += 1 
-
-    while number2 >= 5
+    number += 1 
+    number2 = 2
+    while number2 <= 5
         puts number2
-        count += 1
+        number2 += 2
     end
 end
