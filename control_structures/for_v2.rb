dc924b3b1da0539329ca3378eb841b792745b058
Jonathanbez <jbbarroz@gmail.com>
2023-11-05 20:25:57 -0300

    44nd course OneBitCode

---
+++ control_structures/for_v2.rb
@@ -0,0 +1,15 @@
+#FOR aninhado
+
+number = 1
+
+number2 = 2
+
+while number >= 10
+    puts number 
+    count += 1 
+
+    while number2 >= 5
+        puts number2
+        count += 1
+    end
+end
