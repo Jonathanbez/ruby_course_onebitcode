4c3ad33b156d232bae242f457ade7687d3b432c5
Jonathanbez <jbbarroz@gmail.com>
2023-11-06 07:34:05 -0300

    Collectins Hash course OneBitCode

--- collections/each_hash.rb
+++ collections/each_hash.rb
@@ -1,3 +1,8 @@
+# Uso de EACH em HASH
+# Como sabemos um HASH tem uma chave e o valor da chave
+# Neste caso o HASH "aulas" tem chave (key): "Aula 1"
+# e valor da chave (key) "Aula 1": "liberada"
+
 aulas = {'Aula 1' => 'liberada', 'Aula 2' => 'liberada', 'Aula 3' => 
 'liberada', 'Aula 4' => 'liberada', 'Aula 5' => 'em breve',}
 
