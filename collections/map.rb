cd98470381498f9aa3df1bda27fd68164c30ba08
Jonathanbez <jbbarroz@gmail.com>
2023-11-06 07:55:07 -0300

    Collectins Map course OneBitCode

--- collections/map.rb
+++ collections/map.rb
@@ -14,3 +14,13 @@ puts " #{array}"
 
 puts "\n Novo Array"
 puts " #{new_array}"
+
+puts "\n Executando .map! multiplicando cada item por 2"
+# .map! força que o conteúdo do array original seja alterado"
+
+array.map! do |a|
+    a * 2
+end
+
+puts "\n Array Original"
+puts " #{array}"
