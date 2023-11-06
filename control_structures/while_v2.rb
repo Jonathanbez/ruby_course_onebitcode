95be2c60911b84768941e4b499f4e1ed90770003
Jonathanbez <jbbarroz@gmail.com>
2023-11-06 07:26:04 -0300

    Collectins Hash course OneBitCode

--- control_structures/while_v2.rb
+++ control_structures/while_v2.rb
@@ -2,11 +2,11 @@
 
 number = 1
 
-while number <= 10
+while number <= 100
     puts number 
     number += 1 
     number2 = 2
-    while number2 <= 5
+    while number2 <= 50
         puts number2
         number2 += 2
     end
