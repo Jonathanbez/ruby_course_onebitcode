c7a3192d5e522b5c6638cb01a6e613cb1a6a726c
Jonathanbez <jbbarroz@gmail.com>
2023-11-08 05:36:54 -0300

    Collectins 'Select' course OneBitCode

--- collections/select_array.rb
+++ collections/select_array.rb
@@ -1,11 +1,11 @@
 # SELECT use o .select para selecionar a partir do elemento
 # selecionado (indicado), perceba o '>=', eu tambem poderia 
-# usar '>' (maior) ou somente '=' (igual).
+# usar '>' (maior) ou somente '==' (igual).
 
 array = [1, 2, 3, 4, 5, 6]
 
 selection = array.select do |sl|
-    sl  2
+    sl == 2
 end
 
 puts selection
