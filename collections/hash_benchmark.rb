ede50855bee12cb99670efcec3f6711b9926241a
Jonathanbez <jbbarroz@gmail.com>
2023-11-08 06:59:47 -0300

    Collectins 'Hash Benchmark' course OneBitCode

---
+++ collections/hash_benchmark.rb
@@ -0,0 +1,40 @@
+# Benchmark entre os 3 tipos possiveis de key no Hash
+# Chave (key) com números (1 => "um")
+# Chave (key) com string ("um" => "dois")
+# Chave (key) com simbolo  (:um => "um")
+
+# Hash com chaves (key) de números
+number = {1 => "um", 2 => "dois", 3 => "tres"}
+
+#Hash com chaves (key) de strings
+strings = {"um" => "um", "dois" => "dois", "tres" => "tres"}
+
+#Hash com chaves (key) de simbolo
+symbols = {:um => "um", :dois => "dois", :tres => "tres"}
+
+#Benchmarks dos hash
+puts "Benchmark de Hash com tipos de chave (número, string e simbolo)\n"
+
+puts "Números:\n"
+time = Benchmark.measure do
+    100_000.times do
+        numbers[1]
+    end
+end
+puts "Tempo de execução: #{time}"
+
+puts "String:\n"
+time = Benchmark.measure do
+    100_000.times do
+        strings["um"]
+    end
+end
+puts "Tempo de execução: #{time}"
+
+puts "Símbolo:\n"
+time = Benchmark.measure do
+    100_000.times do
+        symbols[:um]
+    end
+end
+puts "Tempo de execução: #{time}"
