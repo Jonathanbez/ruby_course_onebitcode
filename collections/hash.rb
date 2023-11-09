2f2925854a0ac8465e7c9d522a006ae794bda658
Jonathanbez <jbbarroz@gmail.com>
2023-11-09 07:38:26 -0300

    Collectins 'Exercises Aula 4 collections' course OneBitCode

--- collections/hash.rb
+++ collections/hash.rb
@@ -7,21 +7,21 @@ puts capitais
 
 # Criar um hash podendo já colocar valores armazenados 
 capitais = {}
-puts capitais
+#puts capitais
 
 # Criar um hash já com valores armazenados
 capitais = {acre: 'Rio Branco', sao_paulo: 'São Paulo'}
-puts capitais
+#puts capitais
 
 # hash_x[:chave_valores] = "valores_chave"
 capitais[:minas_gerais] = "Belo Horizonte"
-puts capitais
+#puts capitais
 
 # hash_x.keys para ver as chaves dos valores
 puts capitais.keys
 
 # hash_x.values para ver os valores das chaves
-puts capitais.values
+#puts capitais.values
 
 # hash_x.delete(:chave) para excluir uma chave c/ seu valor do hash
 capitais.delete(:acre)
---
+++ exercises/ex_collection_al4_2.rb
@@ -0,0 +1,17 @@
+
+hash = {}
+
+puts "Digite uma chave para o Hash"
+key = gets.chomp
+
+puts "Digite o valor da chave"
+value = gets.chomp
+
+hash[key] = value
+
+puts hash.key
+puts hash.value
+
+puts "Seu #{hash.key(key)} é #{hash[key]}"
+
+#parei na aula 4 31:02m 
