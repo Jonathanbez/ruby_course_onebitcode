9b10eb02b2ed5f72a5e79e8380df2f718e11b98e
Jonathanbez <jbbarroz@gmail.com>
2023-11-14 08:18:57 -0300

    Exercises Aula 4 collections course OneBitCode

--- exercises/ex_collection_al4_2.rb
+++ exercises/ex_collection_al4_2.rb
@@ -5,10 +5,12 @@ name_age = {}
     puts "Informe seu nome: "
     key = gets.chomp
     puts "Informe sua idade: "
-    name_age[:key] = gets.chomp
+    name_age[key] = gets.chomp
 end
 
 puts "Seu nome é #{key}, e você tem #{name_age[:key]} idade!"
 name_age.each do |key, value|
     puts "Seu nome é #{key}, sua idade é #{value}"
 end
+
+# tentando entender a sintaxe di hash kkk
