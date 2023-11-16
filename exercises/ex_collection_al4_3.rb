2acbd295a3153fc60875e62d9cdf28726a52b20c
Jonathanbez <jbbarroz@gmail.com>
2023-11-16 05:15:05 -0300

    Exercises Aula 4 collections course OneBitCode

--- exercises/ex_collection_al4_3.rb
+++ exercises/ex_collection_al4_3.rb
@@ -1,5 +1,19 @@
 
-number = {A:10}
+number = {A: 10, B: 30, C: 20, D: '25', E: '15'}
 
 puts number.keys
 puts number.values
+
+max_value = 0
+max_key = 0
+
+number.each do |key, value|
+    if value > max_value
+        max_value = value
+        max_key = key
+    end
+end
+puts "maior #{max_value} e mais chave #{max_key}"
+
+
+puts number.values.max
