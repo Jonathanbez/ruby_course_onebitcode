87b07d8d9fb6d8ee1ff7e4e63bd828127419e907
Jonathanbez <jbbarroz@gmail.com>
2023-11-16 07:25:48 -0300

    Exercises Aula 4 collections course OneBitCode

---
+++ methods/method.rb
@@ -0,0 +1,13 @@
+
+def talk
+    puts 'Olá, como você esta?'
+end
+
+talk 
+talk
+
+def talk_name(firt_name, last_name)
+    p "Olá #{firt_name} #{last_name} como você esta?"
+end
+
+talk_name('Jonatham', 'Bezerra')
