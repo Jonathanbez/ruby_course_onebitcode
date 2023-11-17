7915deebd784ab50d0d771404fe42e9891746149
Jonathanbez <jbbarroz@gmail.com>
2023-11-17 07:25:18 -0300

    Methods and Gems aula4 course OneBitCode

--- methods/method.rb
+++ methods/method.rb
@@ -2,7 +2,7 @@
 # Metodos em Ruby, prototipa uma função
 # para ser chamado, podendo ter parametro ou não.
 
-# methods simples
+# Metodo simples
 def talk
     puts "Olá, como você esta?"
 end
@@ -10,9 +10,9 @@ end
 talk 
 talk
 
-# Methods com parametros, lembreçe que ao chamar o methods
-# com parametros, ele ira esperar a quantidade de parametros
-# estabelecida
+# Métodos com parâmetros, lembre-se que ao chamar o método
+# com parâmetros, ele irá esperar a quantidade de parâmetros
+# estabelecida.
 def talk_name(firt_name, last_name)
     p "Olá #{firt_name} #{last_name} como você esta?"
 end
@@ -20,7 +20,9 @@ end
 talk_name('Jonatham', 'Bezerra')
 talk_name("Amanda", "Vitorino")
 
-#
+# Aqui eu defini um valor ao parâmetro do método.
+# Logo, o mesmo irá executar com o valor definido
+# no parâmetro, mas posso alterá-lo.
 def signal(color = 'vermelhor')
     puts "O sinal está #{color}"
 end
