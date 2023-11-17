1806c338b9bbe09fd43dde6558c1c73280f1c9a6
Jonathanbez <jbbarroz@gmail.com>
2023-11-17 07:39:03 -0300

    Methods and Gems aula4 course OneBitCode

--- methods/return.rb
+++ methods/return.rb
@@ -1,5 +1,6 @@
 
-#
+# Com os metodos eu consigo tambem fazer cmparações numericas
+# Consigo relizar calculos e funções predefinidas
 
 def compare(a, b)
     a > b
@@ -8,6 +9,16 @@ end
 a = 1
 b = 2
 
-result = compare (a, b)
+result = compare(a, b)
 
 puts "O resultado da comparação é #{result}"
+
+# Return dentro do metodo nos ajuda a forçar o valor de 
+# return do metodo (função).
+
+def retorno 
+    "Hello, Word!"
+    return "return forçou aqui!"
+end
+
+puts retorno
