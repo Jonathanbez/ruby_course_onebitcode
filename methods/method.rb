b7fc9f5a004bcf12474d4851ce63551e72d24746
Jonathanbez <jbbarroz@gmail.com>
2023-11-17 07:18:25 -0300

    Methods and Gems aula4 course OneBitCode

--- methods/method.rb
+++ methods/method.rb
@@ -10,10 +10,16 @@ end
 talk 
 talk
 
-#m
+# Methods com parametros, lembreçe que ao chamar o methods
+# com parametros, ele ira esperar a quantidade de parametros
+# estabelecida
 def talk_name(firt_name, last_name)
     p "Olá #{firt_name} #{last_name} como você esta?"
 end
 
 talk_name('Jonatham', 'Bezerra')
 talk_name("Amanda", "Vitorino")
+
+#
+def signal(color = 'vermelhor')
+    puts "O sinal"
