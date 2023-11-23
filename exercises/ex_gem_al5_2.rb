7ff306eb385b24efeb8e595dafbfb555575781dd
Jonathanbez <jbbarroz@gmail.com>
2023-11-23 06:41:45 -0300

    Exercices of Methods and Gems aula5 course OneBitCode

--- exercises/ex_gem_al5_2.rb
+++ exercises/ex_gem_al5_2.rb
@@ -6,12 +6,12 @@ require 'cpf_cnpj'
 
 def validate_cpf(cpf)
     if CPF.valid?(cpf, strict: true)
-        p "CPF valido."
+        p 'CPF valido.'
     else
-        p "CPF invalido."
+        p 'CPF invalido.'
     end
 end
 
-p "Informe seu CFP: "
+p 'Informe seu CFP: '
 cpf = gets.chomp
-p validate_cpf(cpf)
+#puts validate_cpf(cpf)
