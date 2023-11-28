500e650fd14377ecc0f86a2b35a626a10f4aa373
Jonathanbez <jbbarroz@gmail.com>
2023-11-28 00:26:08 -0300

    PPO in Ruby

--- PPO_ruby/class.rb
+++
@@ -1,3 +0,0 @@
-
-# Classes em ruby, classe é um tipo de objeto
-# que serve de base/planta/prototipo/tipo para outros obejtos
---
+++ PPO_ruby/computer_class.rb
@@ -0,0 +1,13 @@
+
+# Classes em ruby, classe é um tipo de objeto
+# que serve de base/planta/prototipo/tipo para outros obejtos
+
+class Computer
+    def turn_on
+        'turn on the computer'
+    end
+
+    def shutdown
+        'shutdown the computer'
+    end
+end
