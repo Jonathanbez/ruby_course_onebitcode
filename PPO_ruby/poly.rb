ec80e9d7bc6920d5ac18da4ca4f927d4548773c0
Jonathanbez <jbbarroz@gmail.com>
2023-11-30 07:58:01 -0300

    PPO (class)  in Ruby

--- PPO_ruby/poly.rb
+++ PPO_ruby/poly.rb
@@ -31,6 +31,9 @@ keyboard = Keyboard.new
 lapis = Lapis.new
 caneta = Caneta.new
 
-puts "Teclado", keyboard.escrever
-puts "Lápis", lapis.escrever
-puts  "Caneta", caneta.escrever
+puts "Teclado:"
+keyboard.escrever
+puts "Lápis:"
+lapis.escrever
+puts  "Caneta:"
+caneta.escrever
