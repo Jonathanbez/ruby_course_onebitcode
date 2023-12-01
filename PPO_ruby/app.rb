c89ca7f7c3cb6d6032e065175b4095978c77f732
Jonathanbez <jbbarroz@gmail.com>
2023-12-01 08:01:39 -0300

    PPO II  in Ruby al7

--- PPO_ruby/app.rb
+++ PPO_ruby/app.rb
@@ -1,7 +1,13 @@
 
-#
+# Usando requeri para importar arquivo (usanod o PATH) 
+# para chamar o obejto do outro codigo.
 
-require './animal.rb'
+#require_relative é usado para maior segurança 
+# pois somente carrega o arquivo na mesma pasta ou subdiretorio
+# colocando o caminho, ja require procura em todos os diretorios
+
+#require './animal.rb'
+require_relative 'd/animal'
 
 animal = Animal.new
 animal.dormir
