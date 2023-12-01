e345f9ea857663a89a133edb995267f42ebd9c36
Jonathanbez <jbbarroz@gmail.com>
2023-12-01 06:24:53 -0300

    Exercises of PPO (class)  in Ruby al6

---
+++ exercises/ex_class_al6.rb
@@ -0,0 +1,28 @@
+
+#
+
+class Esportista
+    def competir
+        puts 'Participando de uma competição.'
+    end
+end
+
+esportista = Esportista.new
+
+class JogadorDeFutebol < Esportista
+    def correr
+        puts 'Correndo atrás da bola (lá ele).'
+    end
+end
+
+jogador = JogadorDeFutebol.new
+
+class Maratonista < Esportista
+    def correr
+        puts 'Percorrendo o circuito.'
+    end
+end
+
+maratonista = Maratonista.new
+
+#puts 
