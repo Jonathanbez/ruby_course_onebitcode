ec4631492f85ec7668275c46df7c19a12948a854
Jonathanbez <jbbarroz@gmail.com>
2023-12-03 19:09:43 -0300

    Scope of variables al7

---
+++ scope_variables/local.rb
@@ -0,0 +1,11 @@
+
+# Variavel local, so pode ser acessada
+# dentro do metodo ou classe especifico.
+
+def stay
+    local = 'Esta variavel é acessada apenas dentro deste método'
+    puts local
+end
+
+foo
+
