83efa1faed47a322a80d20e6920a09ae0dbb59a0
Jonathanbez <jbbarroz@gmail.com>
2023-12-03 19:13:23 -0300

    Scope of variables al7

--- scope_variables/local.rb
+++ scope_variables/local.rb
@@ -3,11 +3,11 @@
 # dentro do metodo ou classe especifico.
 
 def stay
-    local = 'Esta variavel é acessada apenas dentro deste método'
+    local = 'Esta variavel é acessada apenas dentro deste método.'
     puts local
 end
 
 stay
 
-local = 'Esta variavel não atrapalha a outra apesar de ter o mesmo nome'
+local = 'Esta variavel não atrapalha a outra apesar de ter o mesmo nome.'
 puts local
