ef2a6370c91efb684fd00849a89af45edd3c4ef8
Jonathanbez <jbbarroz@gmail.com>
2023-12-05 07:03:22 -0300

    Scope of variables al7

---
+++ scope_variables/a.rb
@@ -0,0 +1,25 @@
+
+class Programmer
+  attr_accessor :name, :languages
+  
+  def initialize(name)
+    @name = name
+    @languages = []
+  end
+
+  def learn_language(language)
+    @languages << language
+    puts "#{@name} learned #{language}!"
+  end
+
+  def show_languages
+    puts "#{@name}'s programming languages:"
+    @languages.each { |lang| puts "- #{lang}" }
+  end
+end
+
+# Example Usage
+programmer = Programmer.new('Jonathan')
+programmer.learn_language('Ruby')
+programmer.learn_language('C')
+programmer.show_languages
