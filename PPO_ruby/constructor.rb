9d5f3ebf02a52de2efa18170c6e7deeed90b79ff
Jonathanbez <jbbarroz@gmail.com>
2023-12-05 07:32:50 -0300

    Constructor in Ruby, 'initialize' al7

---
+++ PPO_ruby/constructor.rb
@@ -0,0 +1,14 @@
+
+# Contrutores
+# initialize
+
+class Person
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+    def check
+        puts "Instância da classe iniciada com os valores:"
+        puts "Name = #{@name}"
+        puts "Idade = #{@age}"
