8ec1f209d33d2d51a666ae203e729645ec1c2d96
Jonathanbez <jbbarroz@gmail.com>
2023-12-05 07:37:53 -0300

    Constructor in Ruby, 'initialize' al7

---
+++ PPO_ruby/scope_variables/constructor.rb
@@ -0,0 +1,19 @@
+
+# Contrutores
+# initialize
+
+class Person
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+    def check
+        puts "Instância da classe iniciada com os valores:"
+        puts "Name = #{@name}"
+        puts "Idade = #{@age}"
+    end
+end
+
+person = Person.new('Jonathan', 29)
+person.check
