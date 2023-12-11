8731f6d55fb66b2a476bc5390305be1a23d576a4
Jonathanbez <jbbarroz@gmail.com>
2023-12-11 07:35:42 -0300

    Blocks in Ruby al8

--- blocks/split.rb
+++ blocks/split.rb
@@ -3,7 +3,14 @@
 
 #each sem apreviação do BLOCK
 
+str = "I becoming to a programmer!"
+i = 0
 
+str.split.each do |count|
+    i += 1
+end
+
+puts "The sentence \"#{str}\" have #{i}"
 
 str1 = "I'm learning to program in Ruby!"
 i = 0
