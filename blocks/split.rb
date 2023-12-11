b49b25ffea18550c58688bd9015021f33f4f7a33
Jonathanbez <jbbarroz@gmail.com>
2023-12-11 07:32:39 -0300

    Blocks in Ruby al8

--- blocks/split.rb
+++ blocks/split.rb
@@ -3,9 +3,11 @@
 
 #each sem apreviação do BLOCK
 
-str = "I'm learning to program in Ruby!"
+
+
+str1 = "I'm learning to program in Ruby!"
 i = 0
 
-str.split.each {|count| i += 1 }
+str1.split.each {|count| i += 1 }
 
-puts "Have #{i} words in the sentence \"#{str}\""
+puts "Have #{i} words in the sentence \"#{str1}\""
