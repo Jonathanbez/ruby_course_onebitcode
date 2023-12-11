55edbee8e4284050b96a3c0988c2c0c0906c4c65
Jonathanbez <jbbarroz@gmail.com>
2023-12-10 21:40:56 -0300

    Blocks in Ruby al8

--- blocks/block.rb
+++ blocks/block.rb
@@ -10,5 +10,17 @@ numbers = [5, 10, 5]
 numbers.each {|number| sum += number}
 puts sum
 
-str = "I'm learning programing Ruby"
+str = "I'm learning programing Ruby!"
 puts str
+
+i = 0
+str.split.each do |word|
+    i += 1
+end
+puts "Have #{i} words in the sentence \"#{str}\""
+
+str1 = "Day after day i'm learning more!"
+i = 0
+str1.split.each {|word| i += 1} 
+
+puts "Have #{i} words in the sentence \"#{str1}\""
