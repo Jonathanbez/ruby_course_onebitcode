7cfc30d065f9d69273f8782854c74015948fe1cd
Jonathanbez <jbbarroz@gmail.com>
2023-12-12 07:30:54 -0300

    Blocks in Ruby al8

--- blocks/yield.rb
+++ blocks/yield.rb
@@ -14,3 +14,14 @@ end
 
 y {puts "Exc in block end force yield" 
     puts "..."}
+
+def x
+    if block_given?
+        yield
+    else
+        puts "Sem parâmetro do tipo block"
+    end
+end
+
+x
+x { puts "Comparâmetro do tipo blocl"}
