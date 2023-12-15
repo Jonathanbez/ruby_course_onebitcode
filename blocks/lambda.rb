b3e779f03d2cc4ad7308ebf6559ef03a49261e1d
Jonathanbez <jbbarroz@gmail.com>
2023-12-15 07:28:00 -0300

    Blocks in Ruby al8

--- blocks/lambda.rb
+++ blocks/lambda.rb
@@ -1,5 +1,7 @@
 
-# Lambda
+# Lambda é um tipo de Block que pode ser armazenado em 
+# uma variavel, ou seja um block que se torna um objeto
+# e pode ser armazenado em variavel
 
-first_kiwi = agulha {puts "My first lambda."}
+first_kiwi = lambda {puts "My first lambda."}
 first_kiwi.call
