d24f941a1ece5e2d7bd3216e8ccfe7560387aaa8
Jonathanbez <jbbarroz@gmail.com>
2023-12-22 07:48:18 -0300

    exercises al8, about mixins

--- exercises/ex_lambda_al8.rb
+++ exercises/ex_lambda_al8.rb
@@ -30,7 +30,18 @@ module Person
             puts "cnpj: #{@cnpj}"
         end
     end
+    class Physical
+        def initialize(name, cpf)
+            @name = name
+            @cpf = cpf
+        end
+            def add
+            puts "Pessoa Física Adicionada"
+            puts "nome: #{@name}"
+            puts "cpf: #{@cpf}"
+        end
+    end
 end
 
 Person::Juridic.new('M.C.Investimentos', '4241.123/0001').add
-
+Person::Physical.new('José Almeida', '425.123.123-123').add
