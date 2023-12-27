1f070d55067697447313fbd213e1668922131462
Jonathanbez <jbbarroz@gmail.com>
2023-12-27 08:09:23 -0300

    Regex al9 course Ruby OneBitCode

--- regex/regex.rb
+++ regex/regex.rb
@@ -13,3 +13,8 @@ str = "Meu Deus me ajude a conseguir um emprego como programador"
 puts /ju/ =~ str
 
 puts d = /aju/.match(str)
+
+str1 = "Serei Dev Ruby em 2014!"
+
+puts %r{ev} =~ str1
+puts %r{ev}.match(str1)
