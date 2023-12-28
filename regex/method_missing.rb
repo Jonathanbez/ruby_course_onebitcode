cacff28eb5c177a1996ea7f213e99891cb35d99c
Jonathanbez <jbbarroz@gmail.com>
2023-12-28 06:28:11 -0300

    Method Missing al9 of course Ruby OneBitCode

---
+++ regex/method_missing.rb
@@ -0,0 +1,16 @@
+
+# Method Missing
+
+class Fish
+    def method_missing(method_name)
+        puts "Fish don't have #{method_name} behavior"
+    end
+
+    def swin
+        puts 'Fish is swinming'
+    end
+end
+
+fish = Fish.new
+fish.swin
+fish.walk
