17a3da1cc5550f437bc03a15f12c7d08e92c29ba
Jonathanbez <jbbarroz@gmail.com>
2023-12-30 20:10:43 -0300

    Studies about Proc and lambda in parallel Course Ruby OneBitCode

--- blocks_end_lambdas/proc.rb
+++ blocks_end_lambdas/proc.rb
@@ -11,4 +11,7 @@ times3 = genTimes(3)
 times5 = genTimes(5)
 puts times3.call(12)
 puts times5.call(5)
-puts times3.call(times5.call(4))
+waputs times3.call(times5.call(4))
+
+square = Square.new {|x| x**2}
+puts square.call(3)
