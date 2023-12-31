b5c21a25531f9e13bf5c73d7e39b52048b53f274
Jonathanbez <jbbarroz@gmail.com>
2023-12-30 22:28:37 -0300

    Exercises al9 Course OneBitCode

--- exercises/ex_regexp_al9.rb
+++ exercises/ex_regexp_al9.rb
@@ -13,16 +13,18 @@ puts /\(\d{2,}\) \d{4,}-\d{3,}/.match(text)
 
 class Carro
     def get_km(text)
-        find_km(text)
+        speed = find_km(text)
+        puts "The speed of Fusca is #{speed}"
     end
     private
     def find_km(text)
-        puts /\d{2}km\/h/.match(text)
+        /\d{2}km\/h/.match(text)
     end
 end
 
-text = "Um fusca cor amarelo viaja a 80km/h"
+#text = "Um fusca cor amarelo viaja a 80km/h"
 
 carro = Carro.new
-carro.get_km(text)
+#carro.get_km(text)
+carro.get_km("Um fusca cor amarelo viaja a 80km/h")
     
