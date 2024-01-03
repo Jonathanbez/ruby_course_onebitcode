d7b3799718db7dc4758f76a4583412cec5c33bf1
Jonathanbez <jbbarroz@gmail.com>
2024-01-03 05:42:00 -0300

    Exercises al9 Course OneBitCode

--- regexp_time_private_protected/protected.rb
+++ regexp_time_private_protected/protected.rb
@@ -1,9 +1,10 @@
 
 # Method Protected in Class
-
+=begin
 class Usuario
     def call_protected#(instance)
         puts "#{name} #{surname}"
+        puts instance.name
     end
     protected
     def name
@@ -17,3 +18,20 @@ end
 instance_1 = Usuario.new
 instance_1.call_protected#(instance_1)
 instance_1.name
+=end
+
+class Foo
+    def call_protected(instance)
+        instance.bar
+    end
+    protected
+    def bar
+        puts "protected method"
+    end
+end
+
+instance_1 = Foo.new
+instance_2 = Foo.new
+
+instance_1.call_protected(instance_1)
+instance_1.call_protected(instance_2)
