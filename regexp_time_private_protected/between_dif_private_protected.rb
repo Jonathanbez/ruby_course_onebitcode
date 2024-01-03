6b914a71b5336223ca70027f76c54841d10a18cb
Jonathanbez <jbbarroz@gmail.com>
2024-01-03 06:21:22 -0300

    Exercises al9 Course OneBitCode

---
+++ regexp_time_private_protected/between_dif_private_protected.rb
@@ -0,0 +1,11 @@
+
+# Direnças entre os methods private e protected
+
+class Animal
+    def initialize(name)
+        @name = name
+    end
+
+    def speak
+        puts "Hello, I'm #{@name}"
+    end
