0ea00d9929da0e67b82e7bf9dbb449944c34d6ae
Jonathanbez <jbbarroz@gmail.com>
2024-01-04 07:48:59 -0300

    Class File al10 Course OneBitCode

--- final_project/File.open/create_new_file.rb
+++ final_project/File.open/create_new_file.rb
@@ -1,4 +1,26 @@
 
-File.open('Pai Nosso.txt', 'w') do |file|
+File.open('Pai_Nosso.txt', 'w') do |file|
     file.puts "Pai Nosso que estais nos Céus,"
+end
+
+File.open('Pai_Nosso.txt', 'r') do |file| 
+    file.each {|line| puts line}
+end
+
+File.open('Pai_Nosso.txt', 'a') do |file|
+    file.puts "santificado seja o vosso Nome,"
+    file.puts "venha a nós o vosso Reino,"
+    file.puts "seja feita a vossa vontade"
+    file.puts "assim na terra como no Céu."
+    file.puts "O pão nosso de cada dia nos dai hoje,"
+    file.puts "perdoai-nos as nossas ofensas"
+    file.puts "assim como nós perdoamos"
+    file.puts "a quem nos tem ofendido,"
+    file.puts "e não nos deixeis cair em tentação,"
+    file.puts "mas livrai-nos do Mal."
+    file.puts "Amém!"
+end
+
+File.open('Pai_Nosso.txt') do |file| 
+    file.each {|line| puts line}
 end
