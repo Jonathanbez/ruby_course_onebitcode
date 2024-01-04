498fe4a775773a0536639b6715c69efe3bd70051
Jonathanbez <jbbarroz@gmail.com>
2024-01-04 08:35:13 -0300

    Class File al10 Course OneBitCode

---
+++ final_project/web_call/web_get.rb
@@ -0,0 +1,14 @@
+
+require 'net/http'
+
+url = URI.parse('https://github.com')
+response = Net::HTTP.get_response(url)
+
+if response.is_a?(Net::HTTPSuccess)
+    File.open('exemple.html', 'w') do |line|
+        line.puts(response.body)
+    end
+    puts 'HTML content saved to exemple.html'
+else
+    puts "Failed to retrive HTML. HTTP status code: #{response.code}"
+end
