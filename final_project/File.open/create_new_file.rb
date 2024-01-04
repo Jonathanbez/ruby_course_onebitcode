6dd21cba9e5103334ca71ee57392f15d2ecadb0c
Jonathanbez <jbbarroz@gmail.com>
2024-01-04 07:31:05 -0300

    Class File al10 Course OneBitCode

---
+++ final_project/File.open/create_new_file.rb
@@ -0,0 +1,4 @@
+
+File.open('Pai Nosso.txt', 'w') do |file|
+    file.puts "Pai Nosso que estais nos Céus,"
+end
--- final_project/File.open/write.rb
+++ final_project/File.open/write.rb
@@ -1,5 +1,12 @@
 
+# File.open pode ser usado para escrever string
+# nos aquivos iterando entre eles, no parametro da classe
+# use o nome do arquivo, o o "'a'" para iterar no final
+# da string do arquivo. https://ruby-doc.org/3.3.0/File.html#method-c-open
+# Obs.: Lembrece que puts pula linha, print não.
+
 File.open('shopping-list.txt', 'a') do |line|
+    line.puts("\n")
     line.puts('arroz')
     line.puts('feijão')
     line.print('azeite')
