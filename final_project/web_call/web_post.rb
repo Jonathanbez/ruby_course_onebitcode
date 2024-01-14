afc6677c7096a3ce6075b1ee12e3870e01714702
Jonathanbez <jbbarroz@gmail.com>
2024-01-14 20:47:51 -0300

    Web Get al10 Course OneBitCode

--- final_project/web_call/web_post.rb
+++ final_project/web_call/web_post.rb
@@ -3,10 +3,11 @@ require 'net/http'
 
 req = Net::HTTP::Post.new("/api/users")
 # for make web calls https
-req.set_form_data({name:'Mario', job:'Encanador'})
-response = Net::HTTP.start('reques.in', use_ssl: true) do |http|
-    http.request(req)
+req.set_form_data({ name:'Mario', job:'Encanador' })
+response = Net::HTTP.start('reqres.in', use_ssl: true) do |https|
+    https.request(req)
 end
 
 puts response.code
+puts response.message
 puts response.body
---
+++ final_project/web_call/web_scrap.rb
@@ -0,0 +1,3 @@
+
+# Web Scraping is extraction important informations of the html
+
