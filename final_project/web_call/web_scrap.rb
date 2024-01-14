4714aaebbe221442e2fd0f333d5f3c1073189110
Jonathanbez <jbbarroz@gmail.com>
2024-01-14 20:58:44 -0300

    Web Get al10 Course OneBitCode

--- final_project/web_call/web_scrap.rb
+++ final_project/web_call/web_scrap.rb
@@ -1,3 +1,15 @@
 
 # Web Scraping is extraction important informations of the html
 
+require 'nokogiri'
+require 'net/http'
+
+https = Net::Http.new('onebitcode.com', 443)
+# para fazer chamadas https
+https.use_ssl = true
+
+response = https.gets("/")
+
+doc = Nokogiri::HTML(response.body)
+h1 = doc.at('h1')
+puts h1.content
