8efd283ab91baae7eae63736c24acd396ed387a5
Jonathanbez <jbbarroz@gmail.com>
2024-01-16 06:55:39 -0300

    Web Get al10 Course OneBitCode

--- projects/rates/rates.rb
+++ projects/rates/rates.rb
@@ -2,6 +2,7 @@
 require 'net/http'
 require 'json'
 
+=begin
 # Replace with your Fixer.io API access key
 api_key = gets.chomp
 
@@ -14,11 +15,24 @@ puts "#{rates['USD']}"
 puts "#{rates['BTC']}"
 g = rates['BRL'] / rates['USD']
 puts g
+=end
+
+require 'tty-prompt'
+
+@prompt = TTY::Prompt.new
+
+
+currencies = [
+    {name: "BRL", value: 'BRL'}
+    {name: "USD", value: 'USD'}
+    {name: "BTC", value: 'BTC'}
+]
+
 
 
 # Convert an amount from one currency to another
 amount = 100
-to_currency = 'BRL'
+to_currency = @prompt.select("Select base currency:", currencies)
 
 uri = URI("http://data.fixer.io/api/latest?access_key=#{api_key}&base=EUR&symbols=#{to_currency}")
 response = Net::HTTP.get(uri)
