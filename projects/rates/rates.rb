843804703f8e00067e3469f7dc0f1641340a68b1
Jonathanbez <jbbarroz@gmail.com>
2024-01-16 07:40:21 -0300

    Web Get al10 Course OneBitCode

--- projects/rates/rates.rb
+++ projects/rates/rates.rb
@@ -2,9 +2,8 @@
 require 'net/http'
 require 'json'
 
-=begin
 # Replace with your Fixer.io API access key
-api_key = gets.chomp
+api_key = 
 
 uri = URI("http://data.fixer.io/api/latest?access_key=#{api_key}&base=EUR")
 response = Net::HTTP.get(uri)
@@ -15,29 +14,18 @@ puts "#{rates['USD']}"
 puts "#{rates['BTC']}"
 g = rates['BRL'] / rates['USD']
 puts g
-=end
-
-require 'tty-prompt'
-
-@prompt = TTY::Prompt.new
-
-
-currencies = [
-    {name: "BRL", value: 'BRL'}
-    {name: "USD", value: 'USD'}
-    {name: "BTC", value: 'BTC'}
-]
-
 
 
 # Convert an amount from one currency to another
-amount = 100
-to_currency = @prompt.select("Select base currency:", currencies)
+api_key = 'bab2b7bb14215e41321b35a92269808c'
+amount = 1
+to_currency = 'BRL'
 
 uri = URI("http://data.fixer.io/api/latest?access_key=#{api_key}&base=EUR&symbols=#{to_currency}")
 response = Net::HTTP.get(uri)
+#rates = JSON.parse(response)['rates']
 rate = JSON.parse(response)['rates'][to_currency]
-converted_amount =   rates['USD'] / rate * amount
+converted_amount =   rates['USD'] / rate * 100
 
 puts "#{converted_amount}"
 
