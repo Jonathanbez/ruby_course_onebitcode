c9b9729345b4a50ec9513b4f48d9a811f9d861d7
Jonathanbez <jbbarroz@gmail.com>
2024-01-17 08:42:24 -0300

    final project v1 al10 Course OneBitCode

--- projects/rates/pronpt.rb
+++ projects/rates/pronpt.rb
@@ -12,7 +12,7 @@ puts t_up
 
 puts "Add your API KEY of Fixer"
 api_key = ""
-    while char STDIN.getch
+    while (char = STDIN.getch)
         break if char == "\r" || "\n"
         print "*"
         api_key << char
