8d6feb76b345b34196c322aaf3f17dd88b6ceffd
Jonathanbez <jbbarroz@gmail.com>
2024-01-18 06:16:34 -0300

    final project v1 (rate with Fixer.io al10 Course OneBitCode

--- projects/rates/validate.rb
+++ projects/rates/validate.rb
@@ -2,4 +2,11 @@
 def validate_api_key(api_key)
     uri = URI("http://data.fixer.io/api/latest?access_key=#{api_key}&base=EUR")
     response = Net::HTTP.get_response(uri)
-    
+
+    if response.code.to_i == 200
+        return true
+    else
+        puts "Invalid API Key! Please check and try again."
+        return false
+    end
+end
