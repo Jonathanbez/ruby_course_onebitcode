2ce386d6f5495a5e5897a2426f5cd9a7cde415db
Jonathanbez <jbbarroz@gmail.com>
2024-01-19 05:42:34 -0300

    final project v1 (rate with Fixer.io al10 Course OneBitCode

--- projects/rates/validate.rb
+++ projects/rates/validate.rb
@@ -1,6 +1,6 @@
 
 require 'net/http'
-=begin
+
 module APIKeyValidator
     def self.validate(api_key)
         uri = URI("http://data.fixer.io/api/latest?access_key=#{api_key}&base=EUR")
@@ -14,9 +14,4 @@ module APIKeyValidator
         end
     end
 end
-=end
 
-api_key = 'bab2b7bb14215e41321b35a92269808c'
-uri = URI("http://data.fixer.io/api/latest?access_key=#{api_key}&base=EUR")
-response1 = Net::HTTP.get_response(uri)
-puts response.code
