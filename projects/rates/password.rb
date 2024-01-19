ac8bf96b28fd527ff26e1a98433d2be0b87443a6
Jonathanbez <jbbarroz@gmail.com>
2024-01-19 07:18:12 -0300

    final project v1 (rate with Fixer.io al10 Course OneBitCode

--- projects/rates/password.rb
+++ projects/rates/password.rb
@@ -1,23 +1,16 @@
 
 require 'io/console'
-require 'tty-prompt'
 
 def get_password
     password = ""
 
-    begin
-        IO.console.raw do |io|
-            loop do
-                char = io.getbyte
-                break if char == 13 || char == 10
-                print "F"
-                password << char.chr
+    IO.console.raw do |io|
+        loop do
+            char = io.getbyte
+            break if char == 13 || char == 10
+            print "F"
+            password << char.chr
             end
         end
-    rescue TTY::Reader::InputInterrupt
-        puts "\nPassword entry interrupted. Exiting"
-        exit
-    end
-
     password
 end
