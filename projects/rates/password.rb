f30cb9d9cf22ad9aee2a89d29a7eef4caa07b90a
Jonathanbez <jbbarroz@gmail.com>
2024-01-28 08:04:37 -0300

    final project v1 (rate with Fixer.io al10 Course OneBitCode

--- projects/rates/password.rb
+++ projects/rates/password.rb
@@ -6,8 +6,8 @@ def get_password
 
     IO.console.raw do |io|
         loop do
-            char = io.getbyte
-            break if char == 13 || char == 10
+            char = io.getc
+            break if char == 3 char == 13 || char == 10
             print "F"
             password << char.chr
             end
